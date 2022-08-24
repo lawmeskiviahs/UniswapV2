4b2945a47231b12001dd27d76961810a51eca29f
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-08-24 17:26:11 +0530

    v2Factory inline docs done

--- UniswapV2Factory.sol
+++ UniswapV2Factory.sol
@@ -6,27 +6,29 @@ import './UniswapV2Pair.sol';
 
 
 contract UniswapV2Factory {
-    address public feeTo;
-    address public feeToSetter;
+    address public feeTo;           // address to which the fee would be sent
+    address public feeToSetter;     // address which would set the feeTo address
 
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
+    mapping(address => mapping(address => address)) public getPair;     // mapping of pair contracts and their token pairs
+    address[] public allPairs;                                          // list of all pairs
 
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);  // event emitted at the time of new pair creation
 
-    constructor(address _feeToSetter) {
+    constructor(address _feeToSetter) { // _feeToSetter is provided in input at the time of deployment
         feeToSetter = _feeToSetter;
     }
 
     function allPairsLength() external view returns (uint) {
-        return allPairs.length;
+        return allPairs.length;     // means the total number of pairs created
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
         require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);   // sorting
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+
+        // creating the contract address with seeds kind of like findProgramAddress
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
